efe342f161e990ee439588e606bcb49560927ee7
Charlie Orlando <cmo1991@gmail.com>
2017-03-01 14:12:37 -0600

    continued

--- main.js
+++ main.js
@@ -1,35 +1,67 @@
-turn_counter = 1;
-checker_spot = [];
-current_player = black;
-board = [];
+current_player = 0;
+board = [
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0]];
 
-function fillBoard(){
-  for (var x = 0; x < 7; x++){
-    for (var y = 0; y < 6; y++){
-      board = 
-  }
-}
 
 function newGame(){
-  turn_counter = 1;
+  current_player = 0
   for (var x = 0; x < 7; x++){
     for (var y = 0; y < 6; y++){
       spot = document.createElement("div");
       spot.setAttribute("id", 'spot_'+ x);
       spot.setAttribute("style", 'background:grey');
-      spot.setAttribute("onclick", 'move(this,\''+x+'\')');
+      spot.setAttribute("onclick", 'move(this,\''+x+','+y+'\')');
       document.getElementById('game_container').appendChild(spot);
     }
   }
 }
 
-function move(color, x, y){
-  if (turn_counter % 2 == 0 && color.style.background == 'grey'){
-    color.style.background = 'black';
-    turn_counter++;
+function move(checker,x,y){
+  if (current_player == 0 && checker.style.background == 'grey'){
+    checker.style.background = 'black';
+    current_player = 1;
+    board[x,y] = 'b';
+    checker.innerHTML = board[x,y];
+    checkVictory();
+    console.log(board[x,y]);
   }
-  else if (turn_counter % 2 > 0 && color.style.background == 'grey'){
-      color.style.background = 'red';
-      turn_counter++;
+  else if (current_player == 1 && checker.style.background == 'grey'){
+      checker.style.background = 'red';
+      current_player = 0;
+      board[x,y] = 'r';
+      checker.innerHTML = board[x,y];
+      checkVictory();
     }
+    function checkVictory(){
+      checkHorizontal();
+
+    function checkHorizontal(){
+      current_val = null;
+      previous_val = 0;
+      counter = 0;
+
+      for (var x = 0; x < 7; x++){
+        for (var y = 0; y < 6; y++){
+          current_val = board[x,y];
+          if (current_val === previous_val && current_val !== 0){
+            counter++;
+          } else{
+            counter = 0;
+          }
+          if (counter == 3){
+            alert('BLANK wins');
+          }
+          previous_val = current_val;
+        }
+      counter = 0;
+      previous_val = 0;
+      }
+    }
+
+  }
 }
