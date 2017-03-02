4c86e756a9c77381283ab48f974d45a6674a2c74
Charlie Orlando <cmo1991@gmail.com>
2017-03-02 14:49:08 -0600

    finished game. todo add ai

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ board = [
   [0,0,0,0,0,0,0]];
 b = 1;
 r = 2;
+new_y = null;
 
 
 function newGame(){
@@ -15,7 +16,7 @@ function newGame(){
   for (var y = 0; y < 6; y++){
     for (var x = 0; x < 7; x++){
       spot = document.createElement("div");
-      spot.setAttribute("id", 'spot_'+ x);
+      spot.setAttribute("id", x+','+y);
       spot.setAttribute("style", 'background:grey');
       z = 'this';
       spot.setAttribute("onclick", 'move('+z+','+x+','+y+')');
@@ -25,23 +26,16 @@ function newGame(){
 }
 
 function move(checker,x,y){
-  console.log("x,y",x,y);
-  if (current_player == 0 && checker.style.background == 'grey'){
-    checker.style.background = 'black';
-    current_player = 1;
-    board[y][x] = b;
-  }
-  else if (current_player == 1 && checker.style.background == 'grey'){
-      checker.style.background = 'red';
-      current_player = 0;
-      board[y][x] = r;
-    }
-    checkVictory();
+  checkColumn(x,y);
+  console.log('new_y',new_y);
+  placeChecker(x, new_y);
+  checkVictory(x,y);
 }
 
 function checkVictory(x,y){
   checkHorizontal(x,y);
   checkVertical(x,y);
+  checkDiagonal(x,y);
 }
 
 function checkHorizontal(x,y){
@@ -53,7 +47,6 @@ function checkHorizontal(x,y){
       for (var y = 0; y < 6; y++){
         for (var x = 0; x < 7; x++){
           current_val = board[y][x];
-          console.log('counter',counter);
           if (current_val === previous_val && current_val !== 0){
             counter += 1;
           } else {
@@ -77,7 +70,6 @@ function checkHorizontal(x,y){
       for (var x = 0; x < 7; x++){
         for (var y = 0; y < 6; y++){
           current_val = board[y][x];
-          console.log('counter',counter);
           if (current_val === previous_val && current_val !== 0){
             counter += 1;
           } else {
@@ -92,3 +84,124 @@ function checkHorizontal(x,y){
       previous_val = 0;
       }
     }
+
+    function checkDiagonal(){
+      current_val = null;
+      previous_val = 0;
+      counter = 0;
+      x_temp = 0;
+      y_temp = 0;
+
+      //down-right top
+      for (var x = 0; x < 7; x++){
+        x_temp = x;
+        y_temp = 0;
+
+        while (x_temp < 7 && y_temp < 6){
+          current_val = board[y_temp][x_temp];
+          if (current_val === previous_val && current_val !== 0){
+            counter += 1;
+          } else {
+            counter = 0;
+          }
+          if (counter == 3){
+            alert('win');
+          }
+          previous_val = current_val;
+          x_temp++;
+          y_temp++;
+        }
+      counter = 0;
+      previous_val = 0;
+      }
+
+      //down-left top
+      for (var x = 0; x < 7; x++){
+        x_temp = x;
+        y_temp = 0;
+
+        while (0 <= x_temp  && y_temp < 6){
+          current_val = board[y_temp][x_temp];
+          if (current_val === previous_val && current_val !== 0){
+            counter += 1;
+          } else {
+            counter = 0;
+          }
+          if (counter == 3){
+            alert('win');
+          }
+          previous_val = current_val;
+          x_temp--;
+          y_temp++;
+        }
+      counter = 0;
+      previous_val = 0;
+      }
+
+      //down-right from left
+      for (var y = 0; y < 7; y++){
+        x_temp = 0;
+        y_temp = y;
+
+        while (x_temp < 7 && y_temp < 6){
+          current_val = board[y_temp][x_temp];
+          if (current_val === previous_val && current_val !== 0){
+            counter += 1;
+          } else {
+            counter = 0;
+          }
+          if (counter == 3){
+            alert('win');
+          }
+          previous_val = current_val;
+          x_temp++;
+          y_temp++;
+        }
+      counter = 0;
+      previous_val = 0;
+      }
+
+      //down-left from right
+      for (var y = 0; y < 7; y++){
+        x_temp = 6;
+        y_temp = y;
+
+        while (0 <= x_temp  && y_temp < 6){
+          current_val = board[y_temp][x_temp];
+          if (current_val === previous_val && current_val !== 0){
+            counter += 1;
+          } else {
+            counter = 0;
+          }
+          if (counter == 3){
+            alert('win');
+          }
+          previous_val = current_val;
+          x_temp--;
+          y_temp++;
+        }
+      counter = 0;
+      previous_val = 0;
+      }
+    }
+
+function checkColumn(x,y){
+  for (y = 0; y < 6; y++){
+    if (board[y][x] == 0)
+      new_y = y;
+  }
+}
+
+function placeChecker(x,new_y){
+  div_spot = document.getElementById(x+','+new_y);
+  if (current_player == 0 && div_spot.style.background == 'grey'){
+    div_spot.style.background = 'black';
+    current_player = 1;
+    board[new_y][x] = b;
+  }
+  else if (current_player == 1 && div_spot.style.background == 'grey'){
+      div_spot.style.background = 'red';
+      current_player = 0;
+      board[new_y][x] = r;
+    }
+}
